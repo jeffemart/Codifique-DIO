d20566cf5bdb9f823185159be5bbd8e1a4d7715a
Jefferson <jefferson.martins@online.net.br>
2022-06-20 08:57:21 -0300

    Versionando Template - Projeto DIO

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Nome do produto',
+        productNumber: '012345',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'Nome do produto',
+        productNumber: '012345',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'Nome do produto',
+        productNumber: '012345',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'Nome do produto',
+        productNumber: '012345',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    }
+]
